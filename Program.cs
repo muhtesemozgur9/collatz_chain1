d9dbd45c3158e08718aee45734e885aef5f37940
muhtesemozgur9 <39976050+muhtesemozgur9@users.noreply.github.com>
2018-06-06 05:22:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace collatz_chain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long  best_sayi = 0,zincir=0,best=0;
+            for (long i = 1000000; i > 1; i--)
+            {
+                long n = i;
+                do {
+                    if (n % 2 == 0)
+                    {
+                        n /= 2;
+                        zincir++;
+                    }
+                    else 
+                    {
+                        if (n % 2 == 1 && n != 1)
+                        {
+                            n = n * 3 + 1;
+                            zincir++;
+                        } 
+                    }
+                } while (n != 1);
+                if (best < zincir)
+                {
+                    best = zincir;
+                    best_sayi =i;
+                    zincir = 0;
+                }
+                else zincir =0;
+            }
+            Console.WriteLine("en uzun zincir:"+best);
+            Console.WriteLine("en büyük sayı:"+best_sayi);
+            Console.Read();
+        }
+    }
+}
